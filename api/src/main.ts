7bd7d2c5367ef9d75f3e26d5ee37834cf9ada8c1
cherites <cherites0610@gmail.com>
2026-01-13 21:45:14 +0800

    feat:開放Swagger文檔

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 import { AppModule } from './app.module.js'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter.js'
@@ -23,12 +24,25 @@ async function bootstrap() {
     }),
   )
 
+  const config = new DocumentBuilder()
+    .setTitle('哞福利 API')
+    .setDescription('哞福利系統 API 文件')
+    .addBearerAuth()
+    .addServer('v1')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  // 設定 Swagger 路由為 /api/docs
+  SwaggerModule.setup('api/docs', app, document)
+
   app.useGlobalInterceptors(new TransformInterceptor())
   app.useGlobalFilters(new HttpExceptionFilter())
 
   app.setGlobalPrefix('api/v1')
 
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(port)
 
   console.log(`Application is running on: ${await app.getUrl()}`)
   console.log(`Environment: ${process.env.NODE_ENV}`)
