0a2a45727856570adf747b52a7ae9886045dcd49
cherites <cherites0610@gmail.com>
2026-02-06 01:09:55 +0800

    feat:新增上傳圖片API

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service.js';
+import { Controller, Get, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { AppService } from './app.service.js'
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard.js'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('health-check')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const url = await this.appService.uploadImage(file)
+    return { url }
   }
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Storage } from '@google-cloud/storage'
+import { Injectable } from '@nestjs/common'
+import { extname } from 'path'
 
 @Injectable()
 export class AppService {
+  private storage: Storage
+  private bucketName = 'mou-welfare';
+
+  constructor() {
+    this.storage = new Storage({
+      keyFilename: 'service-account-key.json',
+      projectId: 'welfare-462510',
+    })
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'
+  }
+
+  async uploadImage(file: Express.Multer.File): Promise<string> {
+    const fileName = `${Date.now()}${extname(file.originalname)}`
+    const bucket = this.storage.bucket(this.bucketName)
+    const blob = bucket.file(fileName)
+
+    await blob.save(file.buffer, {
+      contentType: file.mimetype,
+      resumable: false,
+    })
+
+    return `https://storage.googleapis.com/${this.bucketName}/${fileName}`
   }
 }
